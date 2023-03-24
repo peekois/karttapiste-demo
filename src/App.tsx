138604553bbef8c1161b52fa37a9a956aeb254f6
peekois <94543472+peekois@users.noreply.github.com>
2023-03-24 12:59:12 +0200

    Add inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
 import "./App.css";
 import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import axios from "axios";
 
 function App() {
+    const [address, setAddress] = useState({ streetaddress: "", city: "", postalCode: "" });
+
+    function handleInputChange(ev: React.ChangeEvent<HTMLInputElement>) {
+        const { id, value } = ev.target;
+        setAddress((prev) => ({ ...prev, [id]: value }));
+        console.log(address);
+    }
+
     return (
         <div className="map-container">
             <MapContainer className="map" center={[60.183347, 24.939903]} zoom={11.5} maxZoom={18}>
@@ -10,6 +20,35 @@ function App() {
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 />
             </MapContainer>
+            <div className="add-marker">
+                <h2>Lisää karttapiste</h2>
+                <div className="inputs">
+                    <input
+                        type="text"
+                        id="streetaddress"
+                        placeholder="Katuosoite"
+                        value={address.streetaddress}
+                        onChange={(ev) => handleInputChange(ev)}
+                    />
+                    <input
+                        type="number"
+                        id="postalCode"
+                        placeholder="Postinumero"
+                        value={address.postalCode}
+                        onChange={(ev) => handleInputChange(ev)}
+                    />
+                    <input
+                        type="text"
+                        id="city"
+                        placeholder="Kaupunki"
+                        value={address.city}
+                        onChange={(ev) => handleInputChange(ev)}
+                    />
+                </div>
+                <button className="add-button" style={{ width: "10rem" }}>
+                    Lisää
+                </button>
+            </div>
         </div>
     );
 }
