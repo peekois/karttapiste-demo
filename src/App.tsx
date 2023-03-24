539cf8b25b7e4db388402effe893e48c34da5fa9
peekois <94543472+peekois@users.noreply.github.com>
2023-03-24 13:43:14 +0200

    Haversine formula to calculate distance between points

--- src/App.tsx
+++ src/App.tsx
@@ -110,4 +110,21 @@ async function getLocation(address: Address): Promise<Location> {
     return { lat: lat, lng: lng };
 }
 
+// Haversine formula credit: StackOverflow
+function calculateDistance(location1: Location, location2: Location) {
+    const R = 6371; // Radius of the earth in km
+    const dLat = deg2rad(location2.lat - location1.lat);
+    const dLon = deg2rad(location2.lng - location1.lng);
+    const a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(deg2rad(location1.lat)) * Math.cos(deg2rad(location2.lat)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const d = R * c; // Distance in km
+    return d;
+}
+
+function deg2rad(deg: number) {
+    return deg * (Math.PI / 180);
+}
+
 export default App;
