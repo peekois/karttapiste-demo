7907e4c5b140fe20b134de1d59de36d2914ebbe9
peekois <94543472+peekois@users.noreply.github.com>
2023-03-24 12:20:21 +0200

    Add leaflet MapContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 
 function App() {
-    return <div>Haloo??</div>;
+    return (
+        <div className="map-container">
+            <MapContainer className="map" center={[60.183347, 24.939903]} zoom={11.5} maxZoom={18}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
+            </MapContainer>
+        </div>
+    );
 }
 
 export default App;
